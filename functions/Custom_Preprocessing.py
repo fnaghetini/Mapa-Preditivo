1056318718359e6b236c2315db97d78e0886470a
Franco Naghetini <franconaghetini@gmail.com>
2021-06-27 16:26:34 -0300

    Describe Preprocessing section & add figures folder

--- functions/Custom_Preprocessing.py
+++ functions/Custom_Preprocessing.py
@@ -8,8 +8,6 @@ import matplotlib.pyplot as plt
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.decomposition import PCA
 from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import SelectFromModel
-from sklearn.ensemble import RandomForestClassifier
 
 """
     MaskedPCA(n_components :: int, mask :: narray)
@@ -81,56 +79,28 @@ def univariateSelector(X_train, y_train, func):
 # ---------------------------------------------------------------------------------------------------
 
 """
-    randomForestSelector(X_train :: narray, y_train :: narray, split :: string)
+    plotSelectionScores(scores :: narray, features :: list, col :: string, ec :: string)
 
-Retorna os scores de importância das features de acordo com o modelo Random Forest.
-
-Parâmetros:
-- X_train : narray (t, m) das features de treino
-- y_train : narray (t, ) do target de treino
-- split : critério utilizado para medir a qualidade dos splits ('gini' ou 'entropy')
-
-Retorna:
-- X_train_fs : narray(t, n_features) das features de treino selecionadas
-- scores : scores de importância
-
-"""
-
-def randomForestSelector(X_train, y_train, split = 'entropy'):
-    rf_clf = RandomForestClassifier(criterion = split, random_state = 42)
-    n_features = X_train.shape[1]
-    selector = SelectFromModel(estimator = rf_clf, threshold = -np.inf, max_features = n_features)
-    selector.fit(X_train, y_train)
-
-    X_train_fs = selector.transform(X_train)
-    scores = selector.estimator_.feature_importances_
-    
-    return X_train_fs, scores
-
-# ---------------------------------------------------------------------------------------------------
-
-"""
-    plotSelectionScores(score :: narray, feature_labels :: list)
-
-Cria um dicionário com as features e seus respectivos scores de importância.
+Plota um gráfico de barras dos scores de importância de cada umas das features em ordem crescente.
 
 Parâmetros:
-- score : narray (k, ) com os scores das features mais importantes
-- feature_labels : lista com os nomes das features
+- scores : narray (k, ) com os scores das features mais importantes
+- features : lista com os nomes das features
+- col : cor do gráfico de barras
+- ec : cor da borda do gráfico de barras
 
 Retorna:
-- dict : dicionário com k features (chaves) e k scores (valores)
+- Gráfico de barras com os scores de importância de cada uma das features
 
 """
 
-def plotSelectionScores(scores, features, method = None, col = None, ec = None):
+def plotSelectionScores(scores, features, col = None, ec = None):
     
     dic_fs = {'FEATURES' : features, 'SCORES' : list(scores)}
     df_scores = pd.DataFrame(dic_fs).sort_values('SCORES', ascending = False)
     
     plt.figure(figsize = (9,3))
     plt.bar('FEATURES', 'SCORES', data = df_scores, color = col, edgecolor = ec)
-    plt.title(f"Scores de importância - {method}", size = 16)
     plt.ylabel('Score', size = 14)
     plt.yticks(size = 12)
     plt.xticks(rotation = 45, size = 12)
